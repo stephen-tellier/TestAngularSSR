bb66623ed8bec6a30d194464b79290c5385a0941
SRP-STD\s.tellier <stephen.tellier@showroomprive.net>
2022-11-22 16:36:27 +0100

    ajout d'une base de donné json et d'une liste produit

--- angularSSR/src/app/app.module.ts
+++ angularSSR/src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
-import { ProductListModule } from './modules/product-list/product-list.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
